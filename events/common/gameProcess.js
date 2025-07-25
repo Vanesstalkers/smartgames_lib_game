6eff4d34146ea4da451c34ff5695cf0abb8a63fc
VanesStalkers <ivan.mironov@bk.ru>
2025-07-25 23:17:09 +0400

    fix: gameProcess PLAYER_TIMER_END

--- events/common/gameProcess.js
+++ events/common/gameProcess.js
@@ -1,13 +1,13 @@
 () => ({
   name: 'gameProcess',
   handlers: {
-    PLAYER_TIMER_END() {
-      const { game, player } = this.eventContext();
+    PLAYER_TIMER_END({ initPlayer }) {
+      const { game } = this.eventContext();
       game.logs({
         msg: `Игрок {{player}} не успел завершить все действия за отведенное время, и раунд завершится автоматически.`,
-        userId: player.userId,
+        userId: initPlayer.userId,
       });
-      game.run('roundEnd', { timerOverdue: true });
+      game.run('roundEnd', { timerOverdue: true }, initPlayer);
       return { preventListenerRemove: true };
     },
   },
