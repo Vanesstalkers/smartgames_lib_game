8e89fa7b3415f3cceaec739b9f9cf9229e426c0e
VanesStalkers <ivan.mironov@bk.ru>
2025-09-13 15:26:54 +0400

    added custom filter func in api/cards

--- api/cards.js
+++ api/cards.js
@@ -2,10 +2,11 @@
   access: 'public',
   method: async (context, { selectGroup, template } = {}) => {
     if (!template) template = domain.game.configs.cardTemplates.random();
-    const { path, list } = domain.game.configs.cards();
+    const { path, list, filter = () => true } = domain.game.configs.cards();
 
     const cards = list
       .filter((card) => !selectGroup || card.group === selectGroup)
+      .filter(filter)
       .map(path)
       .map((path) => `${template}/${path}`);
 
