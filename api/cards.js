32a6adf88a927fb668844d24e22b902c6b927a2e
VanesStalkers <ivan.mironov@bk.ru>
2025-09-14 15:29:11 +0400

    refactored api/cards

--- api/cards.js
+++ api/cards.js
@@ -2,14 +2,7 @@
   access: 'public',
   method: async (context, { selectGroup, template } = {}) => {
     if (!template) template = domain.game.configs.cardTemplates.random();
-    const { path, list, filter = () => true } = domain.game.configs.cards();
-
-    const cards = list
-      .filter((card) => !selectGroup || card.group === selectGroup)
-      .filter(filter)
-      .map(path)
-      .map((path) => `${template}/${path}`);
-
+    const cards = domain.game.configs.cards({ api: true, selectGroup, template });
     return { status: 'ok', cards };
   },
 });
