733e9652a69be4cacaa2f74437a2f6f9c93923a1
VanesStalkers <ivan.mironov@bk.ru>
2026-01-03 15:27:23 +0400

    prettier

--- actions/loadGame.js
+++ actions/loadGame.js
@@ -1,39 +1,33 @@
 async ({ gameType, gameId, lobbyId, query = {} }) => {
   if (!query._id) query._id = gameId;
 
-  const gameClassGetter = domain.game[gameType]?.class || domain.game.class;
-  return await new gameClassGetter({ id: gameId })
+  const GameClassGetter = domain.game[gameType]?.class || domain.game.class;
+  const processData = ({ loadedInstance: game, loadedData }) => game.run('fillGameData', loadedData);
+
+  return await new GameClassGetter({ id: gameId })
     .load({
-      fromDB: {
-        ...{ id: gameId, query, fromDump: true },
-        processData: function (loadedData) {
-          const game = this;
-          game.run('fillGameData', loadedData);
-        },
-      },
+      fromDB: { id: gameId, query, fromDump: true, processData },
     })
     .then(async (game) => {
       const { deckType, gameType, gameConfig, gameTimer, playerMap } = game;
+
       await lib.store.broadcaster.publishAction.call(game, `lobby-${lobbyId}`, 'addGame', {
         restorationMode: true,
-        ...{ gameId, gameTimer, playerMap },
-        ...{ deckType, gameType, gameConfig },
+        ...{ gameId, gameTimer, playerMap, deckType, gameType, gameConfig },
       });
 
       game.restorationMode = true;
 
       await game.updateGameAtCache({
         restorationMode: true,
-        id: gameId,
-        deckType,
-        gameType,
-        workerId: application.worker.id,
-        port: application.server.port,
+        ...{ id: gameId, deckType, gameType, workerId: application.worker.id, port: application.server.port },
       });
       game.run('initPlayerWaitEvents');
       game.set({ status: 'RESTORING_GAME' }); // в initPlayerWaitEvents выставляется  {status: 'WAIT_FOR_PLAYERS'}
 
-      for (const player of game.players()) player.set({ ready: false });
+      for (const player of game.players()) {
+        player.set({ ready: false });
+      }
 
       return game;
     })
