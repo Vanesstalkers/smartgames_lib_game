05a7116b8bcaead33672e23f4fc31c05491a090e
VanesStalkers <ivan.mironov@bk.ru>
2026-03-21 16:00:44 +0300

    (**) playCard

--- actions/playCard.js
+++ actions/playCard.js
@@ -1,14 +1,27 @@
-(function ({ cardId }, player) {
-  if (player.triggerEventEnabled())
-    throw new Error('Игрок не может совершить это действие, пока не завершит активное событие');
-
+(function ({ cardId, targetPlayerId }, player) {
   const card = this.get(cardId);
 
-  try {
-    card.play({ player, logMsg: `Игрок {{player}} разыграл карту <a>${card.getTitle()}</a>.` });
-    card.moveToTarget(this.decks.active);
-  } catch (error) {
-    this.logs(error.message);
-    player.notifyUser(error.message);
+  if (card.eventData.playDisabled || card.parent().eventData.playDisabled || player.eventData.playDisabled)
+    throw new Error('Эту карту нельзя разыгрывать.');
+
+  if (card.eventData.canReturn) return card.returnToHand({ player });
+  if (card.eventData.activeEvents.length) {
+    for (const event of card.eventData.activeEvents) {
+      event.emit('TRIGGER', { targetId: cardId, targetPlayerId });
+    }
+    return;
   }
+
+  card.moveToTarget(player.decks.played);
+
+  card.set({
+    eventData: {
+      ...{ playedTime: Date.now(), canReturn: true },
+      ...{ cardClass: 'highlight-off', buttonText: 'Вернуть' },
+      restoreState: {
+        buttonText: card.eventData.buttonText || null,
+        cardClass: card.eventData.cardClass || null,
+      },
+    },
+  });
 });
